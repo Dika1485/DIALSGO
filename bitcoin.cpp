1e753c9c12a5cd81b9bd752801ba3d2bdd102ef4
Dika1485 <93853120+Dika1485@users.noreply.github.com>
2022-10-23 22:24:28 +0700

    Update bitcoin.cpp

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -1,35 +1,27 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 int main(){
-    int untung=0,n;
-    bool mark=0;
-    cin>>n;
-    int h[n];
-    cin>>h[0];
-    for(int i=1;i<n;i++){
-        cin>>h[i];
-        if(mark){
-        	if(h[i]<h[i-1]){
-        		mark=!mark;
-        		untung+=h[i-1];
-        	}
-        	else if(i+1==n){
-        		mark=!mark;
-        		untung+=h[i];
-        	}
-        }
-        else{
-        	if(h[i]>h[i-1]){
-        		untung-=h[i-1];
-        		mark=!mark;
-        		if(i+1==n){
-        			mark=!mark;
-        			untung+=h[i];
-        		}
-        	}
-        }
-    }
-    cout<<untung<<endl;
-    return 0;
+	int n,jaw=0;
+	bool beli=1;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+		if(i>0){
+			if(beli){
+				if(a[i]>a[i-1]){
+					jaw-=a[i-1];
+					beli=!beli;
+				}
+			}
+			if(!beli){
+				if(a[i]<a[i-1]){
+					jaw+=a[i-1];
+					beli=!beli;
+				}
+				else if(i==n-1) jaw+=a[i];
+			}
+		}
+	}
+	cout<<jaw<<endl;
 }
